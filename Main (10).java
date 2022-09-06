1f8caeee4c4a37f724d3217fd612e5a669f5ea68
EMMC120605 <112919199+EMMC120605@users.noreply.github.com>
2022-09-06 12:42:15 -0600

    Add files via upload

---
+++ Main (10).java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+   String str = "dos"; 
+    switch(str)
+      {
+        case "uno": 
+          System.out.println("uno");
+          break; 
+        case "dos": 
+          System.out.println("dos"); 
+          break; 
+        case "tres":
+          System.out.println("Tres"); 
+          break; 
+       default: 
+          System.out.println("no coincide"); 
+      }
+    
+    }
+  }
